2c762c15e8d2dae742dd2d4ff10fcd726647f923
GusttavoRodriigues <102168608+GusttavoRodriigues@users.noreply.github.com>
2022-09-07 16:36:42 -0300

    Other components

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,19 @@
-import NotificationButton from "./componentes/NotificationButton"
+import Header from "./componentes/Header"
+import SalesCard from "./componentes/SalesCard"
 
 function App() {
- return(
-
+  return (
     <>
-      <h1>Olá Mundo</h1>
-      <NotificationButton/>
-      <NotificationButton/>
-      <NotificationButton/>
-
-    </>
-
-
- )
+      <Header />
+      <main>
+        <section id="sales">
+          <div className="dsmeta-container">
+          </div>
+            <SalesCard />
+        </section>
+      </main>
+</>
+      )
 }
 
-export default App
+      export default App
